3684472a26dbba3179e41957e26ff151f3a3ef75
ANSozontov <A.N.Sozontov@gmail.com>
2025-11-11 15:02:33 +0500

    data upd

--- Multidimensional_2025-11-10.R
+++ Multidimensional_2025-11-10.R
@@ -1,6 +1,7 @@
 export <- FALSE
 # Data load ---------------------------------------------------------------
 library(tidyverse)
+library(parallel)
 theme_set(
     theme_bw() +
         theme(
@@ -13,6 +14,8 @@ long <- dir("data", pattern = "xlsx") %>%
     sort(decreasing = TRUE) %>% 
     `[`(1) %>% 
     paste0("data/", .) %>% 
+    # преобразовать "sp *" "sp"
+    # перед ординацией убирать всех sp (кроме пронумерованных)
     readxl::read_excel(sheet = 1) %>%  
     group_by(year, tur, zone, site, plot, taxa) %>% 
     summarise(num = sum(num), abu = sum(abu), .groups = "drop") %>% 
@@ -78,6 +81,16 @@ dis.to.df <- function(d, cols = "zone"){
 }
 
 # dis.to.df(dis, c("zone", "km", "plot", "year"))
+sp <- long %>% 
+    pull(taxa) %>% 
+    unique %>% 
+    sort %>% 
+    str_subset("Gen | sp", negate = TRUE)
+
+wide <- wide %>% 
+    select(year:plot, all_of(sp)) %>% 
+    mutate(zone = case_when(zone %in% c("bufer", "background") ~ "control", TRUE ~ "impact")) %>% 
+    arrange(zone)
 
 ref <- wide %>% 
     my_pcoa %>% 
@@ -89,122 +102,61 @@ ref <- wide %>%
     unite("zone", zone1, zone2, sep = "-") %>% 
     pivot_wider(names_from = zone, values_from = mean_dis)
 
-sp <- sort(unique(long$taxa))
+
 
 dif <- sp %>% 
-    `[`(1:5) %>% 
+    # `[`(1:5) %>%
     as.list() %>% 
     `names<-`(sp[1:length(.)]) %>% 
-    map_dfr(
-        ~wide %>% 
-            my_pcoa(sp.to.remove = .x) %>% 
+    mclapply(function(x){
+        wide %>% 
+            my_pcoa(sp.to.remove = x) %>% 
             pluck("dis") %>% 
             dis.to.df() %>% 
             group_by(zone1, zone2) %>% 
             summarise(mean_dis = mean(dis), .groups = "drop") %>% 
             filter(zone1 != zone2) %>%
             unite("zone", zone1, zone2, sep = "-") %>% 
-            pivot_wider(names_from = zone, values_from = mean_dis), 
-        .id = "taxa"
-    )
+            pivot_wider(names_from = zone, values_from = mean_dis)
+        }, 
+        mc.cores = detectCores()-1
+    ) %>% 
+    map_dfr(rbind, .id = "taxa")
+    
+    # map_dfr(
+    #     ~wide %>% 
+    #         my_pcoa(sp.to.remove = .x) %>% 
+    #         pluck("dis") %>% 
+    #         dis.to.df() %>% 
+    #         group_by(zone1, zone2) %>% 
+    #         summarise(mean_dis = mean(dis), .groups = "drop") %>% 
+    #         filter(zone1 != zone2) %>%
+    #         unite("zone", zone1, zone2, sep = "-") %>% 
+    #         pivot_wider(names_from = zone, values_from = mean_dis), 
+    #     .id = "taxa"
+    # )
 
 for(i in 1:ncol(ref)){
-    
+    dif[[i+1]] <- dif[[i+1]] - ref[[i]]
 }
 
+compared <- "impact-control"
+
+dif %>% 
+    select_at(c("taxa", compared)) %>% 
+    arrange_at(compared) %>% 
+    slice(1:8, (nrow(.)-7):nrow(.)) %>% 
     mutate(
-        id1 = factor(paste0(zone1, "_", year1), levels = L, ordered = TRUE),
-        id2 = factor(paste0(zone2, "_", year2), levels = L, ordered = TRUE),
-        .keep = "unused") %>% 
-    split(1:nrow(.)) %>% 
-    lapply(function(x){
-        if(x$id1 > x$id2) {
-            tibble(dis = x$dis, id1 = x$id2, id2 = x$id1)
-        } else {
-            tibble(dis = x$dis, id1 = x$id1, id2 = x$id2)
-        }
-    }) %>% 
-    map_dfr(rbind) %>% 
-    pivot_wider(
-        names_from = id2, values_from = dis, 
-        values_fill = NA, values_fn = mean)
-
-set.seed(1); res$permanova <- vegan::adonis2(
-    dis ~ zone * year, 
-    data = wide, 
-    permutations = 999, 
-    by = "terms")
-
-if(!export){res$permanova}
-
-plots$f4_pcoa <- pc %>% 
-    ggplot(
-        aes(x = Axis.1, y = Axis.2, linetype = year,
-            fill = zone, color = zone, shape = year)) +
-    geom_point(color = "black", size = 2.5) +  
-    stat_ellipse() + 
-    scale_shape_manual(values = c(21, 22))+
-    scale_linetype_manual(values = c("dashed", "solid")) +
-    scale_fill_manual(values = c("darkgreen", "greenyellow", "orange", "red")) +
-    scale_color_manual(values = c("darkgreen", "greenyellow", "orange", "red")) +
-    labs(subtitle = "PCoA\nTurs are united", 
-         x = paste0("Axis 1 (", eig[1], " %)"), 
-         y = paste0("Axis 2 (", eig[2], " %)"), 
-         # fill = "Год", color = "Год", shape = "Год"
-    ) +
-    theme(panel.grid = element_blank())
-if(!export){
-    plots$f4_pcoa
-}
-
-
-# Export ------------------------------------------------------------
-if(export){
-# Fig. S2. Models all
-ggsave(
-    plot = plots$s2_models.all, 
-    filename = paste0("export/Fig.S2_models.all_", Sys.Date(), ".pdf"), 
-    height = 6, width = 11, dpi = 600)
+        taxa = factor(taxa),
+        taxa = fct_inorder(taxa)
+    ) %>% 
+    ggplot(aes(x = taxa, ymin = `impact-control`, ymax = 0)) + 
+    geom_errorbar() + 
+    geom_hline(yintercept = 0, color = "red") + 
+    coord_flip() + 
+    labs(
+        x = NULL,
+        y = paste0("Baseline distance = ", round(ref$`impact-control`, 3))) + 
+    theme(axis.text.y = element_text(face = "italic"))
     
-# Fig. 2. Models selected
-ggsave(
-    plot = plots$f2_models.selected, 
-    filename = paste0("export/Fig.2_models.selected_", Sys.Date(), ".pdf"), 
-    height = 9, width = 11, dpi = 600)
 
-# + Table S3. Models all
-    writexl::write_xlsx(
-        tables$ts3_models.all,
-        paste0("export/Tab.S2_models.selected_", Sys.Date(), ".xlsx")
-    )
-
-# Table 2. Models selected
-writexl::write_xlsx(tables$t2_mod.comps,  paste0("export/Tab.2_models.comp_", Sys.Date(), ".xlsx"))
-# + Fig. 3. Rarefied number of species
-ggsave(
-    paste0("export/Fig.3_rarefication_", Sys.Date(), ".pdf"), 
-    plots$f3_rarefication, 
-    width = 9, height = 5.5, dpi = 600)
-
-# Table 3. Dominant species
-
-# Fig. 4. PCoA ordination
-ggsave(paste0("export/Fig.3_ord_", Sys.Date(), ".pdf"), 
-       plot = plots$f4_pcoa,
-       width = 18, height = 13, units = "cm")
-
-# Permanova in text
-res$permanova %>% 
-    capture.output() %>% 
-    `[`(-c(2, 3, 5, 6, 13, 14)) %>% 
-    as.data.frame() %>% 
-    writexl::write_xlsx(paste0("export/Tab.4a_permanova_", Sys.Date(), ".xlsx"))
-
-# Table 4. Average distances
-if(export){
-    distances %>% 
-        mutate_if(is.numeric, function(a){round(a, 2)}) %>% 
-        writexl::write_xlsx(paste0("export/Tab.4_distances_", Sys.Date(), ".xlsx"))
-}
-
-}
